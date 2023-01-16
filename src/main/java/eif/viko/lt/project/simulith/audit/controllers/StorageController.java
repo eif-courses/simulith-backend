cf212bba39352a20b764d01d87fa6cbf2291c490
Marius <m.gzegozevskis@gmail.com>
2023-01-17 00:34:49 +0200

    Simple updates

--- src/main/java/eif/viko/lt/project/simulith/audit/controllers/StorageController.java
+++ src/main/java/eif/viko/lt/project/simulith/audit/controllers/StorageController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StorageController {
     final FileStorageService fileStorageService;
 
+    @Autowired
     public StorageController(FileStorageService fileStorageService) {
         this.fileStorageService = fileStorageService;
     }
