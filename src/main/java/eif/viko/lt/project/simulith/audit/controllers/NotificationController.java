87be7a98610d7c443ff3e57eae74bf3d48c255aa
Marius <m.gzegozevskis@gmail.com>
2023-01-30 01:24:52 +0200

    Web sockets added

---
+++ src/main/java/eif/viko/lt/project/simulith/audit/controllers/NotificationController.java
@@ -0,0 +1,61 @@
+package eif.viko.lt.project.simulith.audit.controllers;
+
+import eif.viko.lt.project.simulith.audit.models.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import javax.management.Notification;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+public class NotificationController {
+
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+    // Mapped as /app/application
+    @MessageMapping("/application")
+    @SendTo("/all/messages")
+    public Message send(final Message message) throws Exception {
+        return message;
+    }
+
+    // Mapped as /app/private
+    @MessageMapping("/private")
+    public void sendToSpecificUser(@Payload Message message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getTo(), "/specific", message);
+    }
+}
+
+@Component
+@CrossOrigin(origins = "*", maxAge = 3600)
+@EnableWebSocketMessageBroker
+class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/all","/specific");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
--- src/main/java/eif/viko/lt/project/simulith/audit/security/WebSecurityConfig.java
+++ src/main/java/eif/viko/lt/project/simulith/audit/security/WebSecurityConfig.java
@@ -89,6 +89,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authorizeRequests().antMatchers("/api/auth/**").permitAll()
         .antMatchers("/api/test/**").permitAll()
+            .antMatchers("/ws/**").permitAll()
             .antMatchers("/api/storage/**").permitAll()
         .anyRequest().authenticated();
     
